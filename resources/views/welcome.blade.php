3086adde82dcfded34a30de65a8340359b78744c
Simon Davies <simondavies@live.co.uk>
2017-03-27 22:29:00 +0100

    simple page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,12 @@
-blank
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Welcome to the site of Daives</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <p>Somethigns coming soon....</p>
+    </body>
+</html>
